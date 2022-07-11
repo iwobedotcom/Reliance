5ef154fb80595347670bd40d71d186aea1c1b8a2
iwobeearthy <iwobe.aigboje@gmail.com>
2022-07-11 12:28:06 +0100

    Updated package.json

--- next.config.js
+++ next.config.js
@@ -4,4 +4,9 @@ module.exports = {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
